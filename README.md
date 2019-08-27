997155e7cf75660006707c78f368ac41e3e9197a
Lam Du Thach <lamduthach@fabercompany.co.jp>
2019-08-27 18:52:19 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# testMergeError
---
+++ src/main/java/com/thachlam/Main/Main.java
@@ -0,0 +1,16 @@
+package com.thachlam.Main;
+
+/**
+ *
+ * @author Lam Du Thach
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        Main m = new Main();
+    }
+    
+    private String checkUrlResult(String url){
+        return url;
+    }
+}
